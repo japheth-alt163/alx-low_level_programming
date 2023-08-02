58bddfca2e70b081ddd4b993303464bf8b10d059
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-02 09:09:06 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int i;
+	int q;
 	unsigned long int j, k, next, sum;
 
 	j = 1;
 	k = 2;
 	sum = 0;
 
-	for (i = 1; i <= 33; ++i)
+	for (q = 1; q <= 33; ++q)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
