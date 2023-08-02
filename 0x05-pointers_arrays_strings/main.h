4f59414459af101ebac2ce0289167b38ca4f0a0a
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-02 07:29:24 -0700

    int _strlen(char *s);

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-int _putchar(char c);
+int _strlen(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -13,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
-#endif
+#endiint
