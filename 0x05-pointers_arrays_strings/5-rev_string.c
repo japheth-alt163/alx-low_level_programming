7c672219367242f355c093647b41aea6b569075c
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-02 07:39:31 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len, len1;
+	int a, len, len1;
 
 	len = 0;
 	len1 = 0;
@@ -19,10 +19,10 @@ void rev_string(char *s)
 
 	len1 = len - 1;
 
-	for (i = 0; i < len / 2; i++)
+	for (a = 0; a < len / 2; a++)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
+		tmp = s[a];
+		s[a] = s[len1];
 		s[len1--] = tmp;
 	}
 }
