900fd97978b5a1ad6ef5b100bf55482b96d1a229
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-02 08:51:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,23 +6,23 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int q, j;
 
-	i = 0;
+	q = 0;
 
-	while (i < 24)
+	while (q < 24)
 	{
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((q / 10) + '0');
+			_putchar((q % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
 		}
-		i++;
+		q++;
 	}
 }
