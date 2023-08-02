ea5e4df59872cb641eb69b5d21454a8d03e1b6e7
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-02 03:56:27 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * *_puts_recursion - it prints a string followed by a new line
+ * @s: string to to printed
+ *
+ * Return: returns (0)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+}
