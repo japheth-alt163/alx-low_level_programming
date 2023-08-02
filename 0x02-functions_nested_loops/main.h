e1fc97e7f05e4bd97164f2a22e0fc2cc025113b9
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-02 08:27:01 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
-int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +13,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+int _putchar(char c);
 
 #endif
