7fd186476ff196a52836a391695b1899a38260eb
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-10 09:01:20 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,56 +1,31 @@
 #include <stdlib.h>
 #include "main.h"
 
+
 /**
- * array_range - Creates an array of integers from min to max
- * @min: Minimum value (included)
- * @max: Maximum value (included)
- *
- * Return: Pointer to the newly created array, or NULL on failure
+ * array_range - Create an array of integers from min to max.
+ * @min: Minimum value (inclusive).
+ * @max: Maximum value (inclusive).
+ * Return: Pointer to the new array, or NULL on failure or invalid input.
  */
 int *array_range(int min, int max)
 {
 	int *arr;
-	int size, i;
+	int i;
+	int len;
 
 	if (min > max)
 		return (NULL);
 
-	size = max - min + 1;
-	arr = malloc(sizeof(int) * size);
+	len = max - min + 1;
+
+	arr = malloc(sizeof(int) * len);
 
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < len; i++)
 		arr[i] = min++;
 
 	return (arr);
-}
-
-/**
- * main - Entry point of the program
- *
- * This function demonstrates usage of the `array_range` function by creating
- * an array of integers from `min` to `max` and then printing its
- * contents. It also releases the allocated memory after use.
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	int min = 5;
-	int max = 10;
-
-	int *result = array_range(min, max);
-
-	if (result != NULL)
-	{
-		for (int i = 0; i <= max - min; i++)
-			printf("%d ", result[i]);
-
-		free(result);
-	}
-
-	return (0);
 }
