f4f9445c8ba140bf1bf5b909490545fe8a7e3b8f
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-24 08:22:56 -0700

    c is fun

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
@@ -9,17 +10,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
+
+		count++;
 		h = h->next;
-		s++;
 	}
 
-	return (s);
+	return (count);
 }
+
