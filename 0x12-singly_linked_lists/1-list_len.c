6b3ec1eddd9feb60f46906b500099dd750dfde97
japheth-alt163 <japhethbaraka4@gmail.com>
2023-08-24 04:33:52 -0700

    finished

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - Counts the number of elements in a list_t list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elem;
+
+	elem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		elem++;
+	}
+	return (elem);
+}
