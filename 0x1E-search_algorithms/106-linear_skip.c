735b1401ef027bafe88a2d4da533c772442ec0ba
japheth-alt163 <japhethbaraka4@gmail.com>
2024-02-06 22:22:19 -0800

    done

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 
 /**
- * linear_skip - Searches for a value in a sorted skip list of integers
- * @list: Pointer to the head of the skip list to search in
- * @value: The value to search for
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: Pointer to the head of the skip list to search in.
+ * @value: Value to search for.
  *
- * Return: A pointer to the first node where value is located, or NULL if value
- * is not present in the list or if head is NULL
+ * Return: Pointer to the first node where value is located, or NULL if not found.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
@@ -20,29 +19,24 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
     express = list->express;
     current = list;
 
-    while (express)
+    while (express != NULL && express->n < value)
     {
         printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-        if (express->n >= value)
-            break;
-
         current = express;
         express = express->express;
     }
 
-    printf("Value found between indexes [%lu] and [%lu]\n", current->index, express->index);
-
-    while (current)
+    printf("Value found between indexes [%lu] and [%lu]\n", current->index, express ? express->index : current->index);
+    
+    while (current != NULL && current->n < value)
     {
         printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        if (current->n == value)
-            return (current);
-        else if (current->n > value)
-            break;
-
         current = current->next;
     }
 
-    return (NULL);
-}
+    if (current == NULL || current->n != value)
+        return (NULL);
 
+    printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+    return (current);
+}
